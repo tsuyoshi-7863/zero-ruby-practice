658d708841dc37762d013a6e38e878d4a9f47c4b
Tsuyoshi Nishida <nexus703.v6sc.529@gmail.com>
2021-04-20 22:11:59 +0900

    2-3 問6までを解きました。

--- chapter2.rb
+++ chapter2.rb
@@ -1,5 +1,18 @@
 # 2-1
 # 問1
 puts 2 + 3
-# 問２
+# 問2
 puts 2 * 2 * 3.14
+# 2-2
+# 問3
+puts "ruby"
+# 問4
+puts "abc" + "def"
+# 問5
+puts "123".to_i + "456".to_i
+# 2-3
+# 問6
+coffee = 300
+espresso = 100
+puts "コーヒー : #{coffee}円"
+puts "合計 : #{coffee + espresso * 2}円"
